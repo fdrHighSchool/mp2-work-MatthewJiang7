a71ccc7ef77f9dea3d79cee5af8a5dacdff48958
MatthewJiang7 <114923744+MatthewJiang7@users.noreply.github.com>
2022-12-01 08:38:31 -0500

    Create Main.java

---
+++ 03-count-quarters/Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    int a = 25;
+    String num = String.valueOf(a);
+    String cents;
+    if(num.length()<2){
+      cents = num;
+    }
+    else {
+      cents = num.substring(num.length()-2, num.length());
+    }
+    int cients = Integer.parseInt(cents);
+    System.out.println(cients/25);
+  }
+}
